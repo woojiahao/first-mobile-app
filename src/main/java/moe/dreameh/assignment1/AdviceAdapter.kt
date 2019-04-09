d8b48a2a0ba38bf0fec1b27490b72bc4e3bbe42f
Lucas Pentinsaari <kyusaki1@gmail.com>
2019-04-09 13:41:56 +0300

    Added .directory to .gitignore

--- src/main/java/moe/dreameh/assignment1/AdviceAdapter.kt
+++ src/main/java/moe/dreameh/assignment1/AdviceAdapter.kt
@@ -3,17 +3,17 @@ package moe.dreameh.assignment1
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.advice_list_row.view.*
 
 
 class AdviceAdapter(private val adviceList: MutableList<Advice>) : RecyclerView.Adapter<AdviceAdapter.MyViewHolder>() {
 
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var author: TextView = view.findViewById(R.id.author)
-        var content: TextView = view.findViewById(R.id.content)
-        var category: TextView = view.findViewById(R.id.category)
+            val author = view.author
+            val content = view.content
+            val category = view.category
     }
 
     // Essentially set the XML Layout advice_list_row as the layout for a "row"
@@ -26,16 +26,15 @@ class AdviceAdapter(private val adviceList: MutableList<Advice>) : RecyclerView.
 
     // Set ViewHolder variables to the list's item variables
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val advice = adviceList[position]
-        holder.author.text = advice.author
-        holder.category.text = advice.category
-        holder.content.text = advice.content
-
+        val advice = adviceList[position].also { advice ->
+            holder.author.text = advice.author
+            holder.category.text = advice.category
+            holder.content.text = advice.content
+        }
     }
 
     //Get size of advicelist
     override fun getItemCount(): Int {
         return adviceList.size
     }
-
 }
